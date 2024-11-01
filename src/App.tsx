6c72b65f899ab91e5f3fd64d76e77f0cc98df594
Jakub Václavík <jakub.vaclavik.022@pslib.cz>
2024-11-01 09:32:45 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import CocktailSearch from './components/CocktailDb'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <CocktailSearch />
+    </>
+  )
+}
+
+export default App
